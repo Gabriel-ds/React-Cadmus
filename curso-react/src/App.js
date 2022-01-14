59007f08212974d9a88b1e8809f8af12954da4e1
Gabriel <gabrields1910@gmail.com>
2022-01-14 09:14:42 -0300

    Criação do projeto e primeiro desafio: Perfil Osvaldo

---
+++ curso-react/src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/alt-text */
+import './App.css';
+import fotoOsvaldo from './foto-osvaldo.jpeg'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Osvaldo Luis</h1>
+      <img src={fotoOsvaldo}></img>
+      <h3>Programador, formado em Ads</h3>
+
+    </div>
+  );
+}
+
+export default App;
